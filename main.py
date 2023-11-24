2a36dda05d1be409566742140a389bb9f83da5d2
Robin Sardja <robinsardja@gmail.com>
2023-11-24 16:10:40 -0500

    Added function to update players, teams, and games.

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def insertGame():
 # view all games in the games table
 def viewAllGames():
     cur.execute("SELECT date, location, leftteamscore, rightteamscore, t1.name AS leftTeamName, t2.name as rightTeamName FROM games g JOIN \
-        teams t1 ON leftteam = t1.tid JOIN teams t2 ON rightteam = t2.tid")
+        teams t1 ON leftteam = t1.tid JOIN teams t2 ON rightteam = t2.tid ORDER BY date")
     games = cur.fetchall()
 
     table = PrettyTable()
@@ -197,7 +197,7 @@ def insertManager():
 
 # view all team names in the teams table
 def viewAllTeams():
-    cur.execute("SELECT name FROM teams")
+    cur.execute("SELECT name FROM teams ORDER BY name")
     teams = cur.fetchall()
 
     table = PrettyTable()
@@ -216,7 +216,8 @@ def displayTeam():
     team = cur.fetchall()
     #after getting the tid, do input validation and fetch the players
     if team == []:
-        print("This team does not exist")
+        print()
+        print("This team does not exist!")
         return
     cur.execute("SELECT p.pid, p.name, pf.position FROM playsFor pf NATURAL JOIN players p WHERE pf.tid = %s",(team[0][0],))
     players = cur.fetchall()
@@ -231,7 +232,7 @@ def displayTeam():
 
 # view all players in the players table
 def viewAllPlayers():
-    cur.execute("SELECT name FROM players")
+    cur.execute("SELECT name FROM players ORDER BY name")
     teams = cur.fetchall()
 
     table = PrettyTable()
@@ -260,7 +261,8 @@ def displayPlayer():
     cur.execute("SELECT * FROM players WHERE name = %s",(playerName,))
     player = cur.fetchone()
     if player == None:
-        print("This player does not exist")
+        print()
+        print("This player does not exist!")
         return
     table = PrettyTable()
     table.field_names = ["name", "pace", "shooting", "passing", "dribbling", "defending", "physicality", "overall"]
@@ -445,7 +447,6 @@ def removeGame():
 
         print()
         print( "Game successfully deleted!" )
-        return
 
 def removeManager():
     name = input( "What is the name of the manager you wish to delete: " )
@@ -465,6 +466,209 @@ def removeManager():
     print()
     print( "Manager successfully deleted!" )
 
+def updatePlayer():
+    # asks for which player to update
+    playerName = input("Enter the name of the player you would like to update: ")
+    playerName = playerName.strip()
+    cur.execute("SELECT * FROM players WHERE name = %s",(playerName,))
+    player = cur.fetchone()
+    if player == None:
+        print()
+        print("This player does not exist!")
+        return
+    
+    #asks for which attribute of that player to update
+    playerAttribute = input("Which attribute of your selected player would you like to update?: ")
+    playerAttribute = playerAttribute.strip()
+    playerAttribute = playerAttribute.lower()
+    if playerAttribute != "name" and playerAttribute != "pace" and playerAttribute != "shooting" and playerAttribute != "passing" and playerAttribute != "dribbling" and playerAttribute != "defending" and playerAttribute != "physicality" and playerAttribute != "overall":
+        print()
+        print("This is not an attribute of a player!")
+        return
+    
+    newAttributeValue = input("What is the new value of this attribute?: ")
+    newAttributeValue = newAttributeValue.strip()
+
+    # uses single quotes around newAttributeValue for name only because it is a string, all other attributes are integers
+    if playerAttribute == "name":
+        cur.execute("UPDATE players SET {} = '{}' WHERE name = '{}'".format(playerAttribute, newAttributeValue, playerName,) )
+    else:
+        cur.execute("UPDATE players SET {} = {} WHERE name = '{}'".format(playerAttribute, newAttributeValue, playerName,) )
+
+    con.commit()
+
+    print()
+    print( "Player successfully updated!" )
+
+def updateTeam():
+    #asks for which team to update
+    teamName = input("Enter the name of the team you would like to update: ")
+    teamName = teamName.strip()
+    cur.execute("SELECT * FROM teams WHERE name = %s", (teamName,) )
+    team = cur.fetchone()
+    if team == None:
+        print()
+        print("This team does not exist!")
+        return
+    
+    #asks for which attribute of that team to update
+    teamAttribute = input("Which attribute of your selected team would you like to update?: ")
+    teamAttribute = teamAttribute.strip()
+    teamAttribute = teamAttribute.lower()
+    if teamAttribute != "name" and teamAttribute != "league" and teamAttribute != "atkrating"  and teamAttribute != "mdrating" and teamAttribute != "dfrating" and teamAttribute != "overall":
+        print()
+        print( "This is not an attribute of a team!" )
+        return
+    
+    newAttributeValue = input("What is the new value of this attribute?: ")
+    newAttributeValue = newAttributeValue.strip()
+
+    if teamAttribute == "name":
+        cur.execute("UPDATE teams SET {} = '{}' WHERE name = '{}'".format(teamAttribute, newAttributeValue, teamName,) )
+    else:
+        cur.execute("UPDATE teams SET {} = {} WHERE name = '{}'".format(teamAttribute, newAttributeValue, teamName,) )
+
+    con.commit()
+
+    print()
+    print( "Player successfully updated!" )
+
+def updateGame():
+    #asks for which game to update
+    gameLocation = input("Enter the location of the game you would like to update: ")
+    gameLocation = gameLocation.strip()
+    gameLocation = gameLocation.lower()
+    
+    # check if any games were played in this location
+    cur.execute("SELECT * FROM games WHERE LOWER(location) = %s", (gameLocation,) )
+    game = cur.fetchall()
+    if game == []:
+        print()
+        print("No games were ever played in this location!")
+        return
+    elif len(game) == 1:
+        #asks for which attribute of that game to update
+        gameAttribute = input("Which attribute of your selected game would you like to update?: ")
+        gameAttribute = gameAttribute.strip()
+        gameAttribute = gameAttribute.lower()
+        if gameAttribute != "date" and gameAttribute != "location" and gameAttribute != "leftteamscore"  and gameAttribute != "rightteamscore":
+            print()
+            print( "This is not an attribute of a game!" )
+            return
+        
+        newAttributeValue = input("What is the new value of this attribute?: ")
+        newAttributeValue = newAttributeValue.strip()
+
+        if gameAttribute == "date" or gameAttribute == "location":
+            cur.execute("UPDATE games SET {} = '{}' WHERE LOWER(location) = '{}'".format(gameAttribute, newAttributeValue, gameLocation,) )
+        else:
+            cur.execute("UPDATE games SET {} = {} WHERE LOWER(location) = '{}'".format(gameAttribute, newAttributeValue, gameLocation,) )
+
+        con.commit()
+
+        print()
+        print( "Game successfully updated!" )
+        return
+
+    gameYear = input( "What year did this game take place?: " )
+    gameYear = gameYear.strip()
+
+    # check if any games were played in this location in this year
+    cur.execute( "SELECT * FROM games WHERE LOWER(location) = %s AND DATE_PART('year', date) = %s", (gameLocation, gameYear,)  )
+    game = cur.fetchall()
+    if game == []:
+        print()
+        print( "No games were ever played in this location and in this year!" )
+        return
+    elif len(game) == 1:
+        #asks for which attribute of that game to update
+        gameAttribute = input("Which attribute of your selected game would you like to update?: ")
+        gameAttribute = gameAttribute.strip()
+        gameAttribute = gameAttribute.lower()
+        if gameAttribute != "date" and gameAttribute != "location" and gameAttribute != "leftteamscore"  and gameAttribute != "rightteamscore":
+            print()
+            print( "This is not an attribute of a game!" )
+            return
+        
+        newAttributeValue = input("What is the new value of this attribute?: ")
+        newAttributeValue = newAttributeValue.strip()
+
+        if gameAttribute == "date" or gameAttribute == "location":
+            cur.execute("UPDATE games SET {} = '{}' WHERE LOWER(location) = '{}' AND DATE_PART('year', date) = {}".format(gameAttribute, newAttributeValue, gameLocation, gameYear,) )
+        else:
+            cur.execute("UPDATE games SET {} = {} WHERE LOWER(location) = '{}' AND DATE_PART('year', date) = {}".format(gameAttribute, newAttributeValue, gameLocation, gameYear,) )
+
+        con.commit()
+
+        print()
+        print( "Game successfully updated!" )
+        return
+
+    gameMonth = input( "What month did this game take place?: " )
+    gameMonth = gameMonth.strip()
+
+    # check if any games were played in this location in this year and month
+    cur.execute( "SELECT * FROM games WHERE LOWER(location) = %s AND DATE_PART('year', date) = %s AND DATE_PART('month', date) = %s", (gameLocation, gameYear, gameMonth,)  )
+    game = cur.fetchall()
+    if game == []:
+        print()
+        print( "No games were ever played in this location and in this year and month!" )
+        return
+    elif len(game) == 1:
+        #asks for which attribute of that game to update
+        gameAttribute = input("Which attribute of your selected game would you like to update?: ")
+        gameAttribute = gameAttribute.strip()
+        gameAttribute = gameAttribute.lower()
+        if gameAttribute != "date" and gameAttribute != "location" and gameAttribute != "leftteamscore"  and gameAttribute != "rightteamscore":
+            print()
+            print( "This is not an attribute of a game!" )
+            return
+        
+        newAttributeValue = input("What is the new value of this attribute?: ")
+        newAttributeValue = newAttributeValue.strip()
+
+        if gameAttribute == "date" or gameAttribute == "location":
+            cur.execute("UPDATE games SET {} = '{}' WHERE LOWER(location) = '{}' AND DATE_PART('year', date) = {} AND DATE_PART('month', date) = {}".format(gameAttribute, newAttributeValue, gameLocation, gameYear, gameMonth,) )
+        else:
+            cur.execute("UPDATE games SET {} = {} WHERE LOWER(location) = '{}' AND DATE_PART('year', date) = {} AND DATE_PART('month', date) = {}".format(gameAttribute, newAttributeValue, gameLocation, gameYear, gameMonth,) )
+
+        con.commit()
+
+        print()
+        print( "Game successfully updated!" )
+        return
+
+    gameDay = input( "What day did this game take place?: " )
+    gameDay = gameDay.strip()
+
+    # check if any games were played in this location in this year and month
+    cur.execute( "SELECT * FROM games WHERE LOWER(location) = %s AND DATE_PART('year', date) = %s AND DATE_PART('month', date) = %s AND DATE_PART('day', date) = %s", (gameLocation, gameYear, gameMonth, gameDay,)  )
+    game = cur.fetchall()
+    if game == []:
+        print()
+        print( "No games were ever played in this location and in this year and month and day!" )
+        return
+    elif len(game) == 1:
+        #asks for which attribute of that game to update
+        gameAttribute = input("Which attribute of your selected game would you like to update?: ")
+        gameAttribute = gameAttribute.strip()
+        gameAttribute = gameAttribute.lower()
+        if gameAttribute != "date" and gameAttribute != "location" and gameAttribute != "leftteamscore"  and gameAttribute != "rightteamscore":
+            print()
+            print( "This is not an attribute of a game!" )
+            return
+        
+        newAttributeValue = input("What is the new value of this attribute?: ")
+        newAttributeValue = newAttributeValue.strip()
+
+        if gameAttribute == "date" or gameAttribute == "location":
+            cur.execute("UPDATE games SET {} = '{}' WHERE LOWER(location) = '{}' AND DATE_PART('year', date) = {} AND DATE_PART('month', date) = {} AND DATE_PART('day', date)".format(gameAttribute, newAttributeValue, gameLocation, gameYear, gameMonth, gameDay,) )
+        else:
+            cur.execute("UPDATE games SET {} = {} WHERE LOWER(location) = '{}' AND DATE_PART('year', date) = {} AND DATE_PART('month', date) = {} AND DATE_PART('day', date)".format(gameAttribute, newAttributeValue, gameLocation, gameYear, gameMonth, gameDay,) )
+        con.commit()
+
+        print()
+        print( "Game successfully updated!" )
 
 # database connection request. change fields according to your local machine's corresponding value
 con = psycopg2.connect(
@@ -498,6 +702,9 @@ while True:
     print("14. Create a Manager")
     print("15. View all Managers")
     print("16. Delete a Manager")
+    print("17. Update a player")
+    print("18: Update a game")
+    print("19. Update a team")
     print("0. Exit")
     
     print("")
@@ -535,6 +742,12 @@ while True:
         viewAllManagers()
     elif choice == "16":
         removeManager()
+    elif choice == "17":
+        updatePlayer()
+    elif choice == "18":
+        updateGame()
+    elif choice == "19":
+        updateTeam()
     elif choice == "0":
         print("")
         print("Thank you for using FIFAgres! Have a nice day!")
